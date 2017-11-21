b6221d58057b830eed68dd871238b9d6d301318c
JaoChaos <juan.anaya.ortiz@gmail.com>
2017-11-21 10:09:28 +0100

    Arreglo Flask

--- web.py
+++ web.py
@@ -5,14 +5,18 @@ from bson.json_util import dumps
 from bson import json_util
 
 class attend():
-    def attend(message):
+    def attend_info(message):
+        return jsonify({"status":"OK"})
+
+    def attend_command(message):
         return jsonify({"status":"OK"})
 
 @app.route("/")
 def raiz():
     return jsonify({"status":"OK"})
 
-api.add_resource(attend, '/api/attend')
+api.add_resource(attend_info, '/api/attend_info')
+api.add_resource(attend_command, '/api/attend_command')
 
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
