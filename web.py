84163d122f655c8cfd85d3aee8265e8fa375b341
lulivi <luislivilla@gmail.com>
2018-01-28 11:13:21 +0100

    Pep8 formating and tabs error corrected

--- web.py
+++ web.py
@@ -1,11 +1,9 @@
-from flask import Flask,request,jsonify,Response
+from flask import Flask, request, jsonify, Response, render_template
 import json
 import os
 
 app = Flask(__name__)
-{
-   "status": "OK"
-}
+{"status": "OK"}
 """
 {
    "status": "OK",
@@ -15,35 +13,40 @@ app = Flask(__name__)
 }
 """
 
+
 @app.route("/")
 def raiz():
     return render_template('base.html')
 
+
 @app.route("/status")
 def status():
-	data = {"status": "OK"}
+    data = {"status": "OK"}
     return json.dumps(data)
+
 
 class attend():
     def attend_info(message):
         try:
             val = int(message)
-            return jsonify({"status":"OK"})
+            return jsonify({"status": "OK"})
         except ValueError:
             print("That's not an int!")
 
     def attend_command(message):
         try:
             isinstance(message, str)
-            return jsonify({"status":"OK"})
+            return jsonify({"status": "OK"})
         except ValueError:
             print("Thats not a command!")
+
 
 @app.route("/")
 def raiz():
     data = {"status": "OK"}
     return json.dumps(data)
 
+
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
-app.run(host='0.0.0.0', port=port, debug = True, use_reloader = True)
+    app.run(host='0.0.0.0', port=port, debug=True, use_reloader=True)
